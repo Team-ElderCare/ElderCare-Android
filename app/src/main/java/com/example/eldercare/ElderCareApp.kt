22356f40e704c54bd23103bb47b00daabc6e82a7
ësungmin <arcn1345@gmail.com>
2024-12-20 17:35:30 +0900

    [feat/#10] ElderCareApp 추가

--- app/src/main/java/com/example/eldercare/ElderCareApp.kt
+++ app/src/main/java/com/example/eldercare/ElderCareApp.kt
@@ -1,4 +1,25 @@
 package com.example.eldercare
 
-class ElderCareApp {
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.eldercare.util.ElderCareDebugTree
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class ElderCareApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        setTimber()
+        setDarkMode()
+    }
+
+    private fun setTimber() {
+        if (BuildConfig.DEBUG) Timber.plant(ElderCareDebugTree())
+    }
+
+    private fun setDarkMode() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
 }
