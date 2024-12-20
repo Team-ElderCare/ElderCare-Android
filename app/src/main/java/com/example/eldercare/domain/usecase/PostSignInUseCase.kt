fdb184630798c2143ff1c3da8a8261f3c85b3cef
ësungmin <arcn1345@gmail.com>
2024-12-20 17:32:49 +0900

    [feat/#10] usecase 추가

---
+++ app/src/main/java/com/example/eldercare/domain/usecase/PostSignInUseCase.kt
@@ -0,0 +1,9 @@
+package com.example.eldercare.domain.usecase
+
+import com.example.eldercare.domain.repository.AuthRepository
+
+class PostSignInUseCase (
+    private val authRepository: AuthRepository
+) {
+    suspend operator fun invoke() {}
+}
