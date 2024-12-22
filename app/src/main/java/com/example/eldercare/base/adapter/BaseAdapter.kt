012186842c5b9e1bc379ac6c55f2eec7b326c680
Sunghyeon <tjdgustjdan@gmail.com>
2024-12-22 16:34:44 +0900

    [add/#8]: BaseAdapter 코드 추가
    - 뷰 바인딩, onCreateViewHolder 메서드 추가

--- app/src/main/java/com/example/eldercare/base/adapter/BaseAdapter.kt
+++ app/src/main/java/com/example/eldercare/base/adapter/BaseAdapter.kt
@@ -1,13 +1,29 @@
 package com.example.eldercare.base.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
+import androidx.viewbinding.ViewBinding
 import com.example.eldercare.base.diffutil.BaseDiffCallback
 
-abstract class BaseAdapter<T : Any, VH : BaseViewHolder<T>>(
+
+abstract class BaseAdapter<T : Any, VB : ViewBinding, VH : BaseViewHolder<T>>(
     diffCallback: BaseDiffCallback<T>,
 ) : ListAdapter<T, VH>(diffCallback) {
+
+    protected lateinit var binding: VB
+
     // 클릭 리스너 처리를 위한 프로퍼티
     private var itemClickListener: ((T) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (T) -> Unit) {
+        itemClickListener = listener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        binding = inflateBinding(LayoutInflater.from(parent.context), parent, false)
+        return createViewHolder(binding)
+    }
 
     override fun onBindViewHolder(
         holder: VH,
@@ -22,8 +38,15 @@ abstract class BaseAdapter<T : Any, VH : BaseViewHolder<T>>(
             }
         }
 
-        fun setOnItemClickListener(listener: (T) -> Unit) {
-            itemClickListener = listener
-        }
     }
+
+    // ViewBinding inflate를 위한 추상 메서드
+    protected abstract fun inflateBinding(
+        inflater: LayoutInflater,
+        parent: ViewGroup,
+        attachToParent: Boolean
+    ): VB
+
+    // ViewHolder 생성을 위한 추상 메서드
+    protected abstract fun createViewHolder(binding: VB): VH
 }
