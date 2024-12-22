152c81b2118a051065cc256ca6ce2a2f1b2412a4
ësungmin <arcn1345@gmail.com>
2024-12-22 16:59:52 +0900

    [feat/#10] context 확장 함수 추가

---
+++ app/src/main/java/com/example/eldercare/util/context/ContextExt.kt
@@ -0,0 +1,19 @@
+package com.example.eldercare.util.context
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.annotation.ColorRes
+import androidx.annotation.StringRes
+import androidx.core.content.ContextCompat
+
+fun Context.hideKeyboard(view: View) {
+    val inputManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputManager.hideSoftInputFromWindow(view.windowToken, 0)
+    view.clearFocus()
+}
+
+fun Context.stringOf(@StringRes resId: Int) = getString(resId)
+
+fun Context.colorOf(@ColorRes resId: Int) = ContextCompat.getColor(this, resId)
