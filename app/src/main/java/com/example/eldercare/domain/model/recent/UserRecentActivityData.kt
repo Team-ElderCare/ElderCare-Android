39b27197daa02b549e4d3f58654d84c05394cc07
Sunghyeon <tjdgustjdan@gmail.com>
2025-01-15 17:35:07 +0900

    [Style/#21]: KtlintFormat 수정

--- app/src/main/java/com/example/eldercare/domain/model/recent/UserRecentActivityData.kt
+++ app/src/main/java/com/example/eldercare/domain/model/recent/UserRecentActivityData.kt
@@ -1,8 +1,9 @@
 package com.example.eldercare.domain.model.recent
 
+// 감지  타입 : 감지안됨, 약장감지, 감지
 data class UserRecentActivityData(
     val id: Int,
     val time: String,
     val title: String,
-    val sensorType: String, // 감지  타입 : 감지안됨, 약장감지, 감지
+    val sensorType: String,
 )
