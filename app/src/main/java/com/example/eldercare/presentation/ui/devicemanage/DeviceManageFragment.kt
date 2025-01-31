5eca4511d8dd82908cce3a90871906625bd014d3
seognmin <arcn1345@gmail.com>
2025-01-31 17:39:12 +0900

    [chore/#38] 프래그먼트, 이름 변경

--- app/src/main/java/com/example/eldercare/presentation/ui/devicemanage/DeviceManageFragment.kt
+++ app/src/main/java/com/example/eldercare/presentation/ui/devicemanage/DeviceManageFragment.kt
@@ -2,10 +2,10 @@ package com.example.eldercare.presentation.ui.devicemanage
 
 import androidx.lifecycle.ViewModelProvider
 import com.example.eldercare.base.fragment.BaseFragment
-import com.example.eldercare.databinding.FragmentToolBinding
+import com.example.eldercare.databinding.FragmentDeviceManageBinding
 
-class DeviceManageFragment : BaseFragment<FragmentToolBinding, DeviceManageViewModel>(
-    FragmentToolBinding::inflate,
+class DeviceManageFragment : BaseFragment<FragmentDeviceManageBinding, DeviceManageViewModel>(
+    FragmentDeviceManageBinding::inflate,
 ) {
     override val viewModel: DeviceManageViewModel by lazy {
         ViewModelProvider(this)[DeviceManageViewModel::class.java]
