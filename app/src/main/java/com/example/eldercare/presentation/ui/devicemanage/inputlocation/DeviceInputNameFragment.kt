daa0f420741dd5ab3640a1a5e8d0d92dc2f3becc
seognmin <arcn1345@gmail.com>
2025-02-06 00:15:39 +0900

    [feat/#38] 기기 추가 이름 등록 프래그먼트 listener 이벤트 추가

---
+++ app/src/main/java/com/example/eldercare/presentation/ui/devicemanage/inputlocation/DeviceInputNameFragment.kt
@@ -0,0 +1,47 @@
+package com.example.eldercare.presentation.ui.devicemanage.inputlocation
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import com.example.eldercare.base.fragment.BaseFragment
+import com.example.eldercare.databinding.FragmentInputDeviceNameBinding
+
+class DeviceInputNameFragment: BaseFragment<FragmentInputDeviceNameBinding, DeviceInputViewModel> (
+    FragmentInputDeviceNameBinding::inflate
+) {
+    override val viewModel: DeviceInputViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            includeInputDeviceNameTopbar.ivAllTopbarArrowWithTitleArrowLeft.setOnClickListener {
+                parentFragmentManager.popBackStack()
+            }
+            chipInputDeviceNameEnterManually.setOnClickListener {
+                if (chipInputDeviceNameEnterManually.isChecked) {
+                    enableTextField(true)
+                    cgInputDeviceName.clearCheck()
+                    chipInputDeviceNameEnterManually.isChecked = true
+                } else {
+                    enableTextField(false)
+                }
+            }
+            cgInputDeviceName.setOnCheckedStateChangeListener { _, checkedIds ->
+                if (checkedIds.contains(chipInputDeviceNameEnterManually.id)) {
+                    enableTextField(true)
+                } else {
+                    enableTextField(false)
+                }
+            }
+
+            btnInputDeviceNamePrevious.setOnClickListener {
+                parentFragmentManager.popBackStack()
+            }
+        }
+    }
+
+    private fun enableTextField(isVisible: Boolean) {
+        binding.etInputDeviceName.visibility = if (isVisible) View.VISIBLE else View.GONE
+    }
+
+}
