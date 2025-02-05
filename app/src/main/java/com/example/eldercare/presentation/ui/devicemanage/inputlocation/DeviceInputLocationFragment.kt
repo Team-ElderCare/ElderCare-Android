6cbabd03a6cb0085b18ac3139f996322045f3146
seognmin <arcn1345@gmail.com>
2025-02-05 23:43:58 +0900

    [feat/#38] 기기 추가 장소 입력 chip 클릭 이벤트 설정

--- app/src/main/java/com/example/eldercare/presentation/ui/devicemanage/inputlocation/DeviceInputLocationFragment.kt
+++ app/src/main/java/com/example/eldercare/presentation/ui/devicemanage/inputlocation/DeviceInputLocationFragment.kt
@@ -1,5 +1,7 @@
 package com.example.eldercare.presentation.ui.devicemanage.inputlocation
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
 import com.example.eldercare.base.fragment.BaseFragment
 import com.example.eldercare.databinding.FragmentInputDeviceLocationBinding
@@ -8,4 +10,32 @@ class DeviceInputLocationFragment : BaseFragment<FragmentInputDeviceLocationBind
     FragmentInputDeviceLocationBinding :: inflate
 ) {
     override val viewModel: DeviceInputLocationViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            chipInputDeviceLocationEnterManually.setOnClickListener {
+                if (chipInputDeviceLocationEnterManually.isChecked) {
+                    enableTextField(true)
+                    cgInputDeviceLocation.clearCheck()
+                    chipInputDeviceLocationEnterManually.isChecked = true
+                } else {
+                    enableTextField(false)
+                }
+            }
+
+            cgInputDeviceLocation.setOnCheckedStateChangeListener { _, checkedIds ->
+                if (checkedIds.contains(chipInputDeviceLocationEnterManually.id)) {
+                    enableTextField(true)
+                } else {
+                    enableTextField(false)
+                }
+            }
+        }
+    }
+
+    private fun enableTextField(isVisible: Boolean) {
+        binding.etInputDeviceLocation.visibility = if (isVisible) View.VISIBLE else View.GONE
+    }
+
 }
