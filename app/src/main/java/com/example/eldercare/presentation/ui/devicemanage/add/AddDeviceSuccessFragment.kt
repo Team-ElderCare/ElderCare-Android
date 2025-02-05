04a4e7961fe161a148921195acbd38d5d9ccc18b
seognmin <arcn1345@gmail.com>
2025-02-06 00:51:53 +0900

    [feat/#38] 기기 등록 성공 프래그먼트 추가

---
+++ app/src/main/java/com/example/eldercare/presentation/ui/devicemanage/add/AddDeviceSuccessFragment.kt
@@ -0,0 +1,25 @@
+package com.example.eldercare.presentation.ui.devicemanage.add
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import com.example.eldercare.base.fragment.BaseFragment
+import com.example.eldercare.databinding.FragmentAddDeviceSuccessBinding
+import com.example.eldercare.presentation.ui.devicemanage.add.inputlocation.DeviceInputViewModel
+
+class AddDeviceSuccessFragment: BaseFragment<FragmentAddDeviceSuccessBinding, DeviceInputViewModel>(
+    FragmentAddDeviceSuccessBinding::inflate
+) {
+    override val viewModel: DeviceInputViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            includeAddSuccessTopbar.ivAllTopbarArrowWithTitleArrowLeft.setOnClickListener {
+                parentFragmentManager.popBackStack()
+            }
+            // 성공시 기기관리 홈으로 이동해야 함
+        }
+    }
+}
