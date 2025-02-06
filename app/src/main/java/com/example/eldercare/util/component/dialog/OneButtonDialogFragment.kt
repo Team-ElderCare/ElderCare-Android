c761aef000da56a5a46962ffb9641883486d7f1a
seognmin <arcn1345@gmail.com>
2025-02-06 16:45:47 +0900

    [feat/#47] one button dialog 컴포넌트 추가

---
+++ app/src/main/java/com/example/eldercare/util/component/dialog/OneButtonDialogFragment.kt
@@ -0,0 +1,50 @@
+package com.example.eldercare.util.component.dialog
+
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.View
+import com.example.eldercare.R
+import com.example.eldercare.databinding.DialogOneButtonBinding
+
+class OneButtonDialogFragment(
+    private val icon: Int?,
+    private val title: String,
+    private val detail: String,
+    private val buttonText: String,
+    private val clickBtn: () -> Unit,
+    private val onDialogClosed: () -> Unit = {}
+) : BaseDialogFragment<DialogOneButtonBinding>(R.layout.dialog_one_button) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initLayout()
+        addListeners()
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        super.onDismiss(dialog)
+        onDialogClosed()
+    }
+
+    private fun initLayout() {
+        with(binding) {
+            if(icon == null) {
+                ivOneBtnDialogIcon.visibility = View.GONE
+            } else {
+                ivOneBtnDialogIcon.setImageResource(icon)
+            }
+            tvOneBtnDialogTitle.text = title
+            tvOneBtnDialogDetail.text = detail
+            btnOneBtnDialog.text = buttonText
+        }
+    }
+
+    private fun addListeners() {
+        binding.btnOneBtnDialog.setOnClickListener {
+            clickBtn()
+            dismiss()
+        }
+    }
+
+}
