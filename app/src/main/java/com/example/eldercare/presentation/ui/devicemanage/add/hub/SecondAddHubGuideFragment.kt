e89b43a5c878ed6770852a6038b5037a3c05ff72
seognmin <arcn1345@gmail.com>
2025-02-06 19:28:17 +0900

    [feat/#47] 허브 추가 설명 2번 프래그먼트 추가

---
+++ app/src/main/java/com/example/eldercare/presentation/ui/devicemanage/add/hub/SecondAddHubGuideFragment.kt
@@ -0,0 +1,14 @@
+package com.example.eldercare.presentation.ui.devicemanage.add.hub
+
+import android.os.Bundle
+import android.view.View
+import com.example.eldercare.base.fragment.BaseFragment
+import com.example.eldercare.databinding.FragmentAddHubGuideSeconedBinding
+
+class SecondAddHubGuideFragment : BaseFragment<FragmentAddHubGuideSeconedBinding, Nothing>(
+    FragmentAddHubGuideSeconedBinding::inflate
+) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
