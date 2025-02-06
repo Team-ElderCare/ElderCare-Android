fea3105d1151c9fb757f0e969a07d46bb87e42fc
seognmin <arcn1345@gmail.com>
2025-02-06 19:23:40 +0900

    [feat/#47] JVM Target 수정, balloon 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,11 +42,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_18
+        targetCompatibility = JavaVersion.VERSION_18
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "18"
     }
     buildFeatures {
         viewBinding = true
@@ -68,6 +68,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.balloon)
 
     // DataStore
     implementation(libs.androidx.datastore.preferences)
