792b408c38cde61f033c19ce0e6513703b7e4ac9
seognmin <arcn1345@gmail.com>
2025-02-06 19:28:09 +0900

    [feat/#47] 허브 추가 설명 1번 프래그먼트 추가

---
+++ app/src/main/java/com/example/eldercare/presentation/ui/devicemanage/add/hub/FirstAddHubGuideFragment.kt
@@ -0,0 +1,22 @@
+package com.example.eldercare.presentation.ui.devicemanage.add.hub
+
+import android.os.Bundle
+import android.view.View
+import com.example.eldercare.base.fragment.BaseFragment
+import com.example.eldercare.databinding.FragmentAddHubGuideFirstBinding
+
+class FirstAddHubGuideFragment : BaseFragment<FragmentAddHubGuideFirstBinding, Nothing>(
+    FragmentAddHubGuideFirstBinding::inflate
+) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun addListener() {
+        with(binding) {
+            btnAddHubGuideFirstGuideNext.setOnClickListener {
+
+            }
+        }
+    }
+}
