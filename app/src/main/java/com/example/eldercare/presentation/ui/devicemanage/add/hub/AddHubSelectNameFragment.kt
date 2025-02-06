0cdc18fc4f0e920614af1c0d4ddb12e0c1060063
seognmin <arcn1345@gmail.com>
2025-02-06 20:44:10 +0900

    [feat/#47] 허브 등록 장소 선택, 기기 이름 선택 프래그먼트 추가

---
+++ app/src/main/java/com/example/eldercare/presentation/ui/devicemanage/add/hub/AddHubSelectNameFragment.kt
@@ -0,0 +1,52 @@
+package com.example.eldercare.presentation.ui.devicemanage.add.hub
+
+import android.os.Bundle
+import android.view.View
+import com.example.eldercare.base.fragment.BaseFragment
+import com.example.eldercare.databinding.FragmentAddHubSelectNameBinding
+
+class AddHubSelectNameFragment : BaseFragment<FragmentAddHubSelectNameBinding, Nothing>(
+    FragmentAddHubSelectNameBinding::inflate
+){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        addListeners()
+    }
+
+    private fun addListeners() {
+        with(binding) {
+            val cg = includeAddHubSelectNameViewTitleWithCg
+            cg.cgViewTitleWithChipGroup.setOnClickListener {
+                if (cg.chipViewTitleWithChipGroupEnterManually.isChecked) {
+                    enableTextField(true)
+                    cg.cgViewTitleWithChipGroup.clearCheck()
+                    cg.chipViewTitleWithChipGroupEnterManually.isChecked = true
+                } else {
+                    enableTextField(false)
+                }
+            }
+
+            cg.cgViewTitleWithChipGroup.setOnCheckedStateChangeListener { _, checkedIds ->
+                if (checkedIds.contains(cg.chipViewTitleWithChipGroupEnterManually.id)) {
+                    enableTextField(true)
+                } else {
+                    enableTextField(false)
+                }
+            }
+
+            btnAddHubSelectPrevious.setOnClickListener {
+
+            }
+            btnAddHubSelectNext.setOnClickListener {
+
+            }
+        }
+    }
+
+    private fun enableTextField(isVisible: Boolean) {
+        binding.etAddHubSelectName.visibility = if (isVisible) View.VISIBLE else View.GONE
+    }
+
+}
+
---
+++ app/src/main/java/com/example/eldercare/presentation/ui/devicemanage/add/hub/AddHubSelectPlaceFragment.kt
@@ -0,0 +1,51 @@
+package com.example.eldercare.presentation.ui.devicemanage.add.hub
+
+import android.os.Bundle
+import android.view.View
+import com.example.eldercare.base.fragment.BaseFragment
+import com.example.eldercare.databinding.FragmentAddHubSelectPlaceBinding
+
+class AddHubSelectPlaceFragment : BaseFragment<FragmentAddHubSelectPlaceBinding, Nothing>(
+    FragmentAddHubSelectPlaceBinding::inflate
+){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        addListeners()
+    }
+
+    private fun addListeners() {
+        with(binding) {
+            val cg = includeAddHubSelectPlaceViewTitleWithCg
+            cg.cgViewTitleWithChipGroup.setOnClickListener {
+                if (cg.chipViewTitleWithChipGroupEnterManually.isChecked) {
+                    enableTextField(true)
+                    cg.cgViewTitleWithChipGroup.clearCheck()
+                    cg.chipViewTitleWithChipGroupEnterManually.isChecked = true
+                } else {
+                    enableTextField(false)
+                }
+            }
+
+            cg.cgViewTitleWithChipGroup.setOnCheckedStateChangeListener { _, checkedIds ->
+                if (checkedIds.contains(cg.chipViewTitleWithChipGroupEnterManually.id)) {
+                    enableTextField(true)
+                } else {
+                    enableTextField(false)
+                }
+            }
+
+            btnAddHubSelectPrevious.setOnClickListener {
+
+            }
+            btnAddHubSelectNext.setOnClickListener {
+
+            }
+        }
+    }
+
+    private fun enableTextField(isVisible: Boolean) {
+        binding.etAddHubSelectPlace.visibility = if (isVisible) View.VISIBLE else View.GONE
+    }
+
+}
